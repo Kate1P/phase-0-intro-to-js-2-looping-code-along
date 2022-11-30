b81a7539745d6fa3fa8f3480b80d3a8f50eb0224
Kate Popova <popova1ea@gmail.com>
2022-11-29 22:42:52 -0800

    commit tree d74ddb0fcf32289183fddfdd147b9b96a9ab5abd

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+function writeCards(names, surprise){
+    let messages = []
+    for (let i=0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful ${surprise} gift!` ;
+      messages.push(message);
+    }
+return messages;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], 'surprise'));
+
+function countDown() {
+let countdown=10;
+while (countdown >= 0
+    ) {
+    console.log(countdown--);
+}
+
+}
